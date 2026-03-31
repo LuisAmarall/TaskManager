f60c391e0ffe80aca6880af5e5038db9eddd969f
Luis Amaral <olluisamarall@gmail.com>
2026-03-31 13:52:27 -0300

    Creating the DTO interface, basically what the system should do.

---
+++ TaskManager/TaskManager.Application/Interfaces/Services/ITasksServices.cs
@@ -0,0 +1,17 @@
+﻿using TaskManager.Application.Contracts.Requests;
+using TaskManager.Domain.Model;
+
+namespace TaskManager.Application.Interfaces.Services;
+
+public interface ITasksServices
+{
+    Task <Tasks> AddAsync(CreateTasksRequest request);
+
+    Task <Tasks> GetByIdAsync(Guid id);
+
+    Task <IReadOnlyList<Task>> GetAllAsync();
+
+    Task <Tasks> UpdateAsync(Guid id, UpdateTasksRequest request);
+
+    Task <bool> DeleteAsync(Guid id);
+}
